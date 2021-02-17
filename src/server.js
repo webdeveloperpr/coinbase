2ffe4e2c1dc48ce02f29ca3d6dce42bfa329f861
Luis Betancourt <webdeveloperpr@gmail.com>
2021-02-17 01:03:33 -0500

    complete test API

---
+++ src/server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const api = require('./api');
+const actions = require('./actions')(api);
+
+const buyEvent = {
+  "type": "market",
+  "action": "buy",
+  "product_id": "",
+  "funds": "100"
+};
+
+
+const sellEvent = {
+  "type": "market",
+  "action": "sell",
+  // 'BTC-USD
+  "product_id": "",
+  // BTC
+  "currency": "",
+  "size": "0"
+};
+
+app.use(bodyParser.json())
+
+app.post('/buy', function (req, res) {
+  const event = {
+    ...buyEvent,
+    ...req.body
+  }
+
+  actions
+    .buy(event)
+    .then(x => {
+      res.send(x)
+    }).catch(err => {
+      console.log(err);
+      res.send(err)
+    })
+})
+
+app.post('/sell', function (req, res) {
+  const event = {
+    ...sellEvent,
+    ...req.body
+  }
+
+  actions
+    .sell(event)
+    .then(x => {
+      res.send('ok')
+    }).catch(err => {
+      console.log(err);
+      res.send('error')
+    })
+})
+
+app.listen(3000)
